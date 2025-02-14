dc00900d07b1fbf816bb437e27c0dee14566324d
Deba43 <debadatta016@gmail.com>
2025-02-14 11:01:40 +0530

    code

--- src/main/java/org/debadatta/health/service/UserService.java
+++ src/main/java/org/debadatta/health/service/UserService.java
@@ -1,7 +1,5 @@
 package org.debadatta.health.service;
 
-
-
 import java.util.List;
 import java.util.Optional;
 
@@ -13,41 +11,28 @@ import org.debadatta.health.model.Doctors;
 import org.debadatta.health.model.Patients;
 import org.debadatta.health.model.User;
 
-
 public interface UserService {
 
     void saveUser(User user);
 
-   
     void saveAdmin(AdminDto adminDTO);
 
-    
     void saveDoctor(DoctorsDto doctorDTO);
 
     void savePatient(PatientsDto patientDTO);
 
-
-    
     List<User> getAllUsers();
-    
-    
+
     User findByEmail(String email);
 
-    
     Optional<User> findUserById(Long id);
 
     void deleteUserById(Long id);
 
-   
-   
-    
     Optional<Admin> findAdminById(Long id);
 
-    
     Optional<Doctors> findDoctorById(Long id);
 
-    
     Optional<Patients> findPatientById(Long id);
-    
-	
+
 }
